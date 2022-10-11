a1d642cddccac81ac8005d20c4226aeecd67c87c
Anakole <104356239+Anakole@users.noreply.github.com>
2022-10-11 22:29:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,10 +3,10 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
-    form: document.querySelector('.feedback-form');
-    email: document.querySelector('.feedback-form input');
-    massage: document.querySelector('.feedback-form textarea');
-}
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  massage: document.querySelector('.feedback-form textarea'),
+};
 
 refs.form.addEventListener('submit', submitForm);
 refs.form.addEventListener('input', throttle(inputForm));
@@ -14,28 +14,28 @@ refs.form.addEventListener('input', throttle(inputForm));
 let formData = {};
 
 const inputForm = event => {
-    formData[event.tardet.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
+  formData[event.tardet.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
 
 const submitForm = event => {
-    event.preventDefault();
-    event.currentTarget.reset();
+  event.preventDefault();
+  event.currentTarget.reset();
 
-    localStorage.removeItem(STORAGE_KEY);
-    formData = {};
-}
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+};
 
 function populateTextarea() {
-    const formData = localStorage.getItem(STORAGE_KEY);
+  const formData = localStorage.getItem(STORAGE_KEY);
 
-    if (!formData) {
-        return;
-    }
+  if (!formData) {
+    return;
+  }
 
-    Object.entries(formData).forEach(([name, value]) => {
+  Object.entries(formData).forEach(([name, value]) => {
     inputRef.elements[name].value = value;
-    });
+  });
 }
 
 populateTextarea();
